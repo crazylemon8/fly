775dce41ca5d0f0ae71b689596d95f1e1db5845f
crazylemon8 <chawlaaditya7@gmail.com>
2026-04-09 21:27:05 +0530

    Set up initial 2.5D Unity scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 5f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector3(move * speed, rb.linearVelocity.y, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
